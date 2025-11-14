f73e435f12075f0b50ce75165bab2a3c9c6c5e04
Danylo Voloshyn <danilovoloshyn@gmail.com>
2025-11-14 21:40:36 +0200

    Add jsBasics folder

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let stringData = 'JavaScript';
+let numberData = 72;
+let boolData = true;
+let nullData = null;
+let undefiendData = undefined;
+
+console.log(stringData);
+console.log(numberData);
+console.log(boolData);
+console.log(nullData);
+console.log(undefiendData);
