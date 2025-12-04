e07430087c9461bdf413dd6da1c26b94293d5fb2
Danylo Voloshyn <danilovoloshyn@gmail.com>
2025-12-04 22:44:12 +0200

    Add arraysInJS folder

---
+++ arraysInJS/task3.js
@@ -0,0 +1,4 @@
+const numbers = [10, 20, 30, 40, 50];
+const result = numbers.reduce((acc, item) => acc + item);
+
+console.log(result);
---
+++ arraysInJS/task6.js
@@ -0,0 +1,5 @@
+const numbersList = [1,10,14,2,4,5,43,34];
+const sortedCopyList = [...numbersList].sort((a, b) => a - b);
+
+console.log(numbersList);
+console.log(sortedCopyList);
